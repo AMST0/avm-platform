2b9c3c1568242d50b790ac182d1a6fd436932d75
AMST0 <ataberkdudu@gmail.com>
2026-01-15 01:51:00 +0300

    fix: add db fallback url for build time

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,17 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
+            },
+        },
+    })
+}
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
